e5aca715146451da330abf5b7bf193cd6f7b960f
Madhav <Madhavampire@gmail.com>
2025-10-31 03:12:52 +0530

    done

---
+++ csv_to_json.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import json
+import os
+
+from validation import *
+
+# Directories
+INPUT_DIR = "input"
+OUTPUT_DIR = "output"
+# MERGED_DIR = "merged"
+os.makedirs(INPUT_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+# os.makedirs(MERGED_DIR, exist_ok=True)
+
+# Get CSV files
+files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".csv")]
+
+# Convert CSV to JSON
+def csv_to_json(file_name):
+    file_path = os.path.join(INPUT_DIR, file_name)
+    df = pd.read_csv(file_path)
+    return df.to_dict(orient="records")
+
+
+# Merge and filter data
+merged_json = []
+for f in files:
+    data_json = csv_to_json(f)
+    for entry in data_json:
+        if pd.notna(entry.get("*Customer First Name")) and pd.notna(entry.get("*Customer Mobile")):
+            merged_json.append(entry)
+
+# # Save merged JSON
+# with open(os.path.join(MERGED_DIR, "merged.json"), 'w', encoding='utf-8') as file:
+#     json.dump(merged_json, file, ensure_ascii=False, indent=4)
+
+
+
+series = {}
+
+# assign order ids
+for it, entry in enumerate(merged_json):
+    print(it, entry["*Product Name"].strip().split(" "))
+    entry["*Order Id"] = get_order_id(entry, series)
+    entry["*Customer Mobile"] = make_valid_ph_(entry["*Customer Mobile"])
+    entry["Order Date as dd-mm-yyyy hh:MM"] = get_today()
+    entry["*Master SKU"] = entry["*Product Name"]
+    entry["*Partial COD (Yes/No)"] = "No"
+    entry["Customer Alternate Mobile"] = make_valid_alternate_ph_num(
+        entry["*Customer Mobile"],
+        entry["Customer Alternate Mobile"]
+    )
+    entry["*Shipping Address Line 1"] = make_valid_addr(entry["*Shipping Address Line 1"])
+    series[entry["*Product Name"].lower()] += 1
+
+
+# validation
+invalid_entries = []
+merged_json_valid_entries = []
+
+for entry in merged_json:
+    if not correct_ph_num(entry["*Customer Mobile"]):
+        invalid_entries.append(entry)
+        print(f"\n\n\nPrimary\n\n\n")
+        continue
+    if not correct_ph_num(entry["Customer Alternate Mobile"]):
+        invalid_entries.append(entry)
+        print(f"\n\n\nAlternate\n\n\n")
+        continue
+    if not correct_pincode(entry["*Shipping Address Postcode"]):
+        print(f"\n\n\nPostcode\n\n\n")
+        invalid_entries.append(entry)
+        continue
+    if len(entry["*Shipping Address Line 1"]) >= 180:
+        invalid_entries.append(entry)
+        continue
+    
+    merged_json_valid_entries.append(entry)
+
+
+
+# Save as valid CSV
+df_valid = pd.json_normalize(merged_json_valid_entries)
+# df_valid = df_valid[order]
+df_valid.to_csv(os.path.join(OUTPUT_DIR, f"Order Upload File Valid {len(merged_json_valid_entries)}.csv"), index=False)
+
+
+# Save as invalid CSV
+# with open(os.path.join(OUTPUT_DIR, f"Invalid {len(invalid_entries)}.json"), 'w', encoding='utf-8') as file:
+#     file.write(str(invalid_entries))
+
+
+
+df_invalid = pd.json_normalize(invalid_entries)
+# df_invalid = df_invalid[order]
+df_invalid.to_csv(os.path.join(OUTPUT_DIR, f"Order Upload File Valid {len(merged_json_valid_entries)}.csv"), index=False)
+
---
+++ validation.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+import re
+import pandas as pd
+
+# 
+def get_today():
+    return datetime.now().strftime("%d-%m-%Y %H:%M")
+
+# 
+def get_day():
+    today = get_today()
+    today_obj = datetime.strptime(today, "%d-%m-%Y %H:%M")
+    return today_obj.strftime("%d%b%y").upper()
+
+# 
+lang_codes = {
+    'english': 'EN', 'hindi': 'HI', 'kannada': 'KA',
+    'telugu': 'TL', 'tamil': 'TA', 'malayalam': 'MA',
+    'marathi': 'MH', 'gujarati': 'GJ', 'punjabi': 'PB',
+    'bengali': 'BG', 'nepali': 'NE', 'odia': 'OD', 'odiya': 'OD',
+    'assamese': 'AS', 'manipuri': 'MN', 'bodo': 'BO',
+    'urdu': 'UR', 'sindhi': 'SN'
+}
+
+# 
+def correct_ph_num(ph_num_str):
+    if isinstance(ph_num_str, str):
+        if ph_num_str.lower() == "nan": return False
+    try:
+        ph_num_str = float(ph_num_str)
+    except:
+        pass
+    if not ph_num_str: return True
+    ph_num_str = str(int(ph_num_str))
+    return len(ph_num_str) == 10
+
+
+def make_valid_addr(addr_str):
+    if "#" in addr_str:
+        addr_str = addr_str[addr_str.index("#"):]
+    pattern = r"Pin \d{6} Ph \d{10}"
+    addr_str = re.sub(pattern, "", addr_str)
+    return addr_str
+
+
+def correct_pincode(pin):
+    return len(str(pin).replace('.0', '')) == 6
+
+
+
+def make_valid_ph_(num):
+    if not pd.notna(num):
+        return ""
+    num = str(int(num))
+    return num
+    
+    
+    
+def make_valid_alternate_ph_num(primary_num, alternate_num):
+    if not pd.notna(alternate_num):
+        return ""
+    nums = str(alternate_num).strip().split(",")
+    for i in nums:
+        x = str(int(i.replace('.0', '')))
+        if x != str(int(primary_num)):
+            return i
+    return nums[0]
+
+#
+def get_order_id(entry, series: dict):
+    # KAGGTM30OCT2500470
+    order_id = "KA"
+
+    # print(entry["*Product Name"].strip().split(" "))
+    bookname = entry["*Product Name"].split(" ")[0]
+    lang = entry["*Product Name"].split(" ")[1]
+    order_id += bookname + lang_codes[lang.lower()]
+
+    day = get_day()
+    order_id += day
+
+    if entry["*Product Name"].lower() not in series:
+        series[entry["*Product Name"].lower()] = 0
+    sno = series[entry["*Product Name"].lower()] + 1
+    series_id = f"{sno:05}"
+    order_id += series_id
+
+    return order_id
+
+order = [
+    "*Order Id",
+    "Order Date as dd-mm-yyyy hh:MM",
+    "*Channel",
+    "*Payment Method(COD/Prepaid)",
+    "*Customer First Name",
+    "Customer Last Name",
+    "Email (Optional)",
+    "*Customer Mobile",
+    "Customer Alternate Mobile",
+    "*Shipping Address Line 1",
+    "Shipping Address Line 2",
+    "*Shipping Address Country",
+    "*Shipping Address State",
+    "*Shipping Address City",
+    "*Shipping Address Postcode",
+    "Billing Address Line 1",
+    "Billing Address Line 2",
+    "Billing Address Country",
+    "Billing Address State",
+    "Billing Address City",
+    "Billing Address Postcode",
+    "*Master SKU",
+    "*Product Name",
+    "*Product Quantity",
+    "Tax %",
+    "*Selling Price(Per Unit Item, Inclusive of Tax)",
+    "Discount(Per Unit Item)",
+    "Shipping Charges(Per Order)",
+    "COD Charges(Per Order)",
+    "Gift Wrap Charges(Per Order)",
+    "Total Discount (Per Order)",
+    "*Partial COD (Yes/No)",
+    "*Length (cm)",
+    "*Breadth (cm)",
+    "*Height (cm)",
+    "*Weight Of Shipment(kg)",
+    "Send Notification(True/False)",
+    "Comment",
+    "HSN Code",
+    "Location Id",
+    "Reseller Name",
+    "Company Name",
+    "latitude",
+    "longitude",
+    "Verified Order",
+    "Is documents",
+    "Order Type",
+    "Order tag",
+]
+
+
